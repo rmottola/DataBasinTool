1b2570bfb0915d9442b2691a11fb745028f20101
Riccardo Mottola <rm@gnu.org>
2021-05-04 02:44:26 +0200

    initial project

---
+++ main.m
@@ -0,0 +1,23 @@
+/*
+   Project: DataBasinTool
+
+   Author: multix
+
+   Created: 2021-05-04 02:39:34 +0200 by multix
+*/
+
+#import <Foundation/Foundation.h>
+
+int
+main(int argc, const char *argv[])
+{
+  id pool = [[NSAutoreleasePool alloc] init];
+
+  // Your code here...
+
+  // The end...
+  [pool release];
+
+  return 0;
+}
+
